9886ea56808497f100bf37126bce9985b1dd1d9d
Ferhat Kortak <ferhat.kortak@masomo.com>
2020-06-13 12:36:31 +0300

    feat: Add new quiz page.

---
+++ DeuQuiz/View Controller/QuizDescViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuizDescViewController.swift
+//  DeuQuiz
+//
+//  Created by Ferhat Kortak on 13.06.2020.
+//  Copyright © 2020 og. All rights reserved.
+//
+
+import UIKit
+
+class QuizDescviewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
