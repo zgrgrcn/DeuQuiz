6c552f532d41e15030bf7a0f137f357ea9d3e983
Ferhat Kortak <ferhat.kortak@masomo.com>
2020-06-13 20:53:40 +0300

    feat: Next question button.

--- DeuQuiz/View Controller/MakeQuizVC.swift
+++ DeuQuiz/View Controller/MakeQuizVC.swift
@@ -14,12 +14,20 @@ class MakeQuizVC: UIViewController {
     @IBOutlet weak var radioButton3: UIButton!
     @IBOutlet weak var radioButton4: UIButton!
     
+    @IBOutlet weak var txtQuestion: UITextView!
+    
+    @IBOutlet weak var txtOption1: UITextField!
+    @IBOutlet weak var txtOption2: UITextField!
+    @IBOutlet weak var txtOption3: UITextField!
+    @IBOutlet weak var txtOption4: UITextField!
+    
+    override func viewDidLoad() {
+        radioButton1.isSelected = true
+    }
+    
     @IBAction func radioButtonSwitch(_ sender: UIButton) {
         
-        radioButton1.isSelected = false
-        radioButton2.isSelected = false
-        radioButton3.isSelected = false
-        radioButton4.isSelected = false
+        clearRadioButtons()
         
         switch sender.tag {
             case 1:
@@ -35,4 +43,21 @@ class MakeQuizVC: UIViewController {
         }
     }
     
+    func clearRadioButtons(){
+        radioButton1.isSelected = false
+        radioButton2.isSelected = false
+        radioButton3.isSelected = false
+        radioButton4.isSelected = false
+    }
+    
+    @IBAction func nextQuestionTap(_ sender: Any) {
+        txtQuestion.text = ""
+        txtOption1.text = ""
+        txtOption2.text = ""
+        txtOption3.text = ""
+        txtOption4.text = ""
+        
+        clearRadioButtons()
+        radioButton1.isSelected = true
+    }
 }
