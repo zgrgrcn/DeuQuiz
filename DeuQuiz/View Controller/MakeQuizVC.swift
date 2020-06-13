ad033c8bc246a0a90e5df5ba803f1d06b127be82
Ferhat Kortak <ferhat.kortak@masomo.com>
2020-06-13 20:46:17 +0300

    feat: Add radio buttons.

--- DeuQuiz/View Controller/MakeQuizVC.swift
+++ DeuQuiz/View Controller/MakeQuizVC.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class MakeQuizVC: UIViewController {
-    @IBOutlet weak var TEST: UIButton!
+    @IBOutlet weak var radioButton1: UIButton!
+    @IBOutlet weak var radioButton2: UIButton!
+    @IBOutlet weak var radioButton3: UIButton!
+    @IBOutlet weak var radioButton4: UIButton!
     
-    @IBAction func test1(_ sender: Any) {
-        print("Apple Sucks")
+    @IBAction func radioButtonSwitch(_ sender: UIButton) {
+        
+        radioButton1.isSelected = false
+        radioButton2.isSelected = false
+        radioButton3.isSelected = false
+        radioButton4.isSelected = false
+        
+        switch sender.tag {
+            case 1:
+                radioButton1.isSelected = true
+            case 2:
+                radioButton2.isSelected = true
+            case 3:
+                radioButton3.isSelected = true
+            case 4:
+                radioButton4.isSelected = true
+        default:
+            print("An error has occured")
+        }
     }
-    @IBAction func deneme(_ sender: Any) {
-        print("Appleın mk")
-    }
+    
 }
