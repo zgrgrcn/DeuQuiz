200818713e400d8f9ac53d675bad1430a8133b02
Ferhat Kortak <ferhat.kortak@masomo.com>
2020-06-13 20:29:08 +0300

    fix apple problems.

---
+++ DeuQuiz/View Controller/MakeQuizVC.swift
@@ -0,0 +1,20 @@
+//
+//  TestViewController.swift
+//  DeuQuiz
+//
+//  Created by Ferhat Kortak on 13.06.2020.
+//  Copyright © 2020 og. All rights reserved.
+//
+
+import UIKit
+
+class MakeQuizVC: UIViewController {
+    @IBOutlet weak var TEST: UIButton!
+    
+    @IBAction func test1(_ sender: Any) {
+        print("Apple Sucks")
+    }
+    @IBAction func deneme(_ sender: Any) {
+        print("Appleın mk")
+    }
+}
---
+++ DeuQuiz/View Controller/QuizInfoVC.swift
@@ -0,0 +1,13 @@
+//
+//  QuizInfoVC.swift
+//  DeuQuiz
+//
+//  Created by Ferhat Kortak on 13.06.2020.
+//  Copyright © 2020 og. All rights reserved.
+//
+
+import UIKit
+
+class QuizInfoVC: UIViewController {
+    
+}
