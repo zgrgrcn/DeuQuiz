929babdf19a25132881eff55af31c32e49850ad0
Ferhat Kortak <ferhat.kortak@masomo.com>
2020-06-14 00:39:02 +0300

    feat: Add alert for inproper inputs to add new quiz page.

--- DeuQuiz/View Controller/QuizInfoVC.swift
+++ DeuQuiz/View Controller/QuizInfoVC.swift
@@ -21,17 +21,32 @@ class QuizInfoVC: UIViewController {
     }
     
     @IBAction func btnMakeQuiz(_ sender: Any) {
-        var title = quizTitle.text;
-        var totalDuration = Int(duration.text!) ?? 10;
-        var code = enterCode.text;
-        var userID = Auth.auth().currentUser?.uid
         
-        // Fill the entity
-        QuizEntity.getInstance().title = title!
-        QuizEntity.getInstance().duration = totalDuration
-        QuizEntity.getInstance().enterCode = code!
-        QuizEntity.getInstance().teacherID = userID!
+
         
+        let isProperInput = quizTitle.text!.count>2 && duration.text!.count>0 && enterCode.text?.count == 6
+        
+        if  !isProperInput{
+            
+            // create the alert
+            let alert = UIAlertController(title: "Warning!", message: "Please fill the inputs properly.", preferredStyle: UIAlertController.Style.alert)
+                   // add an action (button)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                   // show the alert
+                   self.present(alert, animated: true, completion: nil)
+            
+        }else{
+            var title = quizTitle.text;
+            var totalDuration = Int(duration.text!) ?? 10;
+            var code = enterCode.text;
+            var userID = Auth.auth().currentUser?.uid
+            
+            // Fill the entity
+            QuizEntity.getInstance().title = title!
+            QuizEntity.getInstance().duration = totalDuration
+            QuizEntity.getInstance().enterCode = code!
+            QuizEntity.getInstance().teacherID = userID!
+        }
     }
     
 }
